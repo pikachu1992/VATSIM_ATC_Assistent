37ad81bff61984c9c191b366b87816e313777f92
Tiago Vicente <tmavicente@gmail.com>
2019-02-19 01:56:00 +0000

    Add project files.

---
+++ VATSIM_ATC_Assistent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VATSIM_ATC_Assistent
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
